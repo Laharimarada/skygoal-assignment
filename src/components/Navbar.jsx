56f01610b3c23d2b149c186f61cd4f8eac7f56e4
Laharimarada <laharimarada02@gmail.com>
2023-11-11 18:02:03 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,30 +3,32 @@ import { useState } from "react";
 const Navbar = () => {
   const [state, setState] = useState(false);
   return (
-    <nav className="nav-bar">
-      <div className="nav-first-group">
-        <div>Home</div>
-        <div>About</div>
-        <div>Schedules</div>
-        <div>Pricing</div>
-      </div>
-      <div className="nav-sec-group">
-        <div style={{ color: "#7D7D7D" }}>Offers</div>
-        <button className="btn-orange">Courses</button>
-      </div>
-      <div className="emptybox"></div>
-      <div className="hamburger" onClick={() => setState(!state)}>
-        <div className="ham-line"></div>
-        <div className="ham-line"></div>
-        <div className="ham-line"></div>
-      </div>
-      <div className={"mobile-nav " + (state ? "flex" : "none")}>
-        <div>Home</div>
-        <div>About</div>
-        <div>Schedules</div>
-        <div>Pricing</div>
-      </div>
-    </nav>
+    <div className="nav-fix">
+      <nav className="nav-bar">
+        <div className="nav-first-group">
+          <div>Home</div>
+          <div>About</div>
+          <div>Schedules</div>
+          <div>Pricing</div>
+        </div>
+        <div className="nav-sec-group">
+          <div style={{ color: "#7D7D7D" }}>Offers</div>
+          <button className="btn-orange">Courses</button>
+        </div>
+        <div className="emptybox"></div>
+        <div className="hamburger" onClick={() => setState(!state)}>
+          <div className="ham-line"></div>
+          <div className="ham-line"></div>
+          <div className="ham-line"></div>
+        </div>
+        <div className={"mobile-nav " + (state ? "flex" : "none")}>
+          <div>Home</div>
+          <div>About</div>
+          <div>Schedules</div>
+          <div>Pricing</div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
